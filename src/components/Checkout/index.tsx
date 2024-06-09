7759a0b9a0c8458cff867bfa06e10166dd144378
Ari P <ariteknologi@gmail.com>
2024-06-09 22:21:43 +0700

    fix minor

--- src/components/Checkout/index.tsx
+++ src/components/Checkout/index.tsx
@@ -1,9 +1,27 @@
+import { Card, CardBody, Tab, Tabs } from "@nextui-org/react"
+import { useTranslation } from "react-i18next"
 import CheckoutOrder from "./CheckoutOrder";
 
 const Checkout = () => {
 
+    const { t } = useTranslation();
+
     return (
-        <CheckoutOrder />
+        <Tabs aria-label="Options" variant="underlined" classNames={{
+            cursor:"bg-[#ff5a00]",
+            base: "justify-center"
+        }}>
+            <Tab key="order" title={t('checkout.order')}>
+                <CheckoutOrder />
+            </Tab>
+            <Tab key="review" title={t('checkout.review')}>
+                <Card>
+                    <CardBody>
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </CardBody>
+                </Card>  
+            </Tab>
+        </Tabs>
     )
 }
 
