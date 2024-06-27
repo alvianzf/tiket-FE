26ff52bfbe8c5d902db00692e6383774173b992a
Ari P <ariteknologi@gmail.com>
2024-06-27 14:00:20 +0700

    fix minor

--- src/components/Payment/PaymentSuccess.tsx
+++ src/components/Payment/PaymentSuccess.tsx
@@ -1,10 +1,10 @@
-import { BookFlight } from "@api/bookFlight/types";
+import { GetBookFlightResponse } from "@api/bookFlight/types";
 import Button from "@components/Button";
 import { Image } from "@nextui-org/react";
 import { useTranslation } from "react-i18next";
 
 interface Props {
-    flight?: BookFlight;
+    flight?: GetBookFlightResponse;
 }
 
 const PaymentSuccess = ({ flight } :Props) => {
@@ -17,7 +17,7 @@ const PaymentSuccess = ({ flight } :Props) => {
                 <Image src="/images/payment-success.png" width={200} height={200}/>
             </div>
             <p className="text-orange text-lg font-medium">{t('checkout.payment_received')}</p>
-            <p>{t('checkout.order_no', { no : flight?.kodebooking })}</p>
+            <p>{t('checkout.order_no', { no : flight?.data.bookingCode })}</p>
             <p className="text-orange text-lg font-medium">{t('checkout.thankyou_for_your_payment')}</p>
             <Button bgColor={"orange"}>
                 {t('checkout.get_eticket')}
