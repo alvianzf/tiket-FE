4c952df5f1d900518cb6a18ccdf5bdc238668512
alvianzf <alvianzf@gmail.com>
2024-10-13 22:22:30 +0700

    fix:breaking error when inputting very old year (edge cases)

--- src/components/Checkout/CheckoutOrderAdult.tsx
+++ src/components/Checkout/CheckoutOrderAdult.tsx
@@ -3,12 +3,10 @@ import { useFieldArray, useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { FormProps } from "./forms/useForm";
 import moment from "moment";
-import { parseDate } from "@internationalized/date";
+import { parseDate, CalendarDate } from "@internationalized/date";
 
 const CheckoutOrderAdult = () => {
-
     const { setValue, formState: { errors }, control, watch } = useFormContext<FormProps>();
-
     const { t } = useTranslation();
 
     const { fields } = useFieldArray({
@@ -23,82 +21,92 @@ const CheckoutOrderAdult = () => {
         { key: 'Ms', label: t('checkout.ms') },
     ];
 
+    const handleDateChange = (value: Date | null, index: number) => {
+        if (value) {
+            setValue(`adultPassengers.${index}.date_of_birth`, moment(value).format('YYYY-MM-DD'));
+        } else {
+            console.error("Invalid date selected");
+        }
+    };
+
+    const parseDateSafely = (dateString: string | undefined): CalendarDate | undefined => {
+        if (!dateString) return undefined;
+        try {
+            return parseDate(dateString);
+        } catch (error) {
+            console.error("Error parsing date:", error);
+            return undefined;
+        }
+    };
+
     return (
-        fields.map((field, index) => (
-            <Card key={index} classNames={{
-                header: "font-medium"
-            }}>
-                <CardHeader>
-                    {t('checkout.adult', { count: index+1 })}
-                </CardHeader>
-                <Divider />
-                <CardBody>
-                    <div className="flex flex-col gap-5 w-full">
-                        <div className="flex flex-row gap-2 items-center">
-                            <p className="w-[50%]">{t('checkout.name_middle_name')}</p>
-                            <div className="flex flex-row gap-2 w-full">
-                                <Select
-                                    className="max-w-xs"
-                                    variant="bordered"
-                                    radius="sm"
-                                    selectionMode="single"
-                                    placeholder={t('checkout.choose')}
-                                    selectedKeys={[watch(`adultPassengers.${index}.call`)]}
-                                    errorMessage={errors?.adultPassengers?.[index]?.call?.message}
-                                    isInvalid={!!errors?.adultPassengers?.[index]?.call}
-                                    onChange={(e) => setValue(`adultPassengers.${index}.call`, e.target.value)}
-                                >
-                                    {options.map((item) => (
-                                            <SelectItem key={item.key}>
+        <>
+            {fields.map((field, index) => (
+                <Card key={field.adultPassengerKey} className="font-medium">
+                    <CardHeader>
+                        {t('checkout.adult', { count: index+1 })}
+                    </CardHeader>
+                    <Divider />
+                    <CardBody>
+                        <div className="flex flex-col gap-5 w-full">
+                            <div className="flex flex-row gap-2 items-center">
+                                <p className="w-[50%]">{t('checkout.name_middle_name')}</p>
+                                <div className="flex flex-row gap-2 w-full">
+                                    <Select
+                                        className="max-w-xs"
+                                        variant="bordered"
+                                        radius="sm"
+                                        selectionMode="single"
+                                        placeholder={t('checkout.choose')}
+                                        selectedKeys={[watch(`adultPassengers.${index}.call`)]}
+                                        errorMessage={errors?.adultPassengers?.[index]?.call?.message}
+                                        isInvalid={!!errors?.adultPassengers?.[index]?.call}
+                                        onChange={(e) => setValue(`adultPassengers.${index}.call`, e.target.value)}
+                                    >
+                                        {options.map((item) => (
+                                            <SelectItem key={item.key} value={item.key}>
                                                 {item.label}
                                             </SelectItem>
-                                        ))
-                                    }
-                                </Select>
+                                        ))}
+                                    </Select>
+                                    <Input
+                                        type="text"
+                                        variant="bordered"
+                                        value={watch(`adultPassengers.${index}.firstname`)}
+                                        onValueChange={(value) => setValue(`adultPassengers.${index}.firstname`, value)}
+                                        isInvalid={!!errors?.adultPassengers?.[index]?.firstname}
+                                        errorMessage={errors?.adultPassengers?.[index]?.firstname?.message}
+                                        className="rounded-none w-full"
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-row gap-2 items-center">
+                                <p className="w-[50%]">{t('checkout.last_name')}</p>
                                 <Input
                                     type="text"
                                     variant="bordered"
-                                    defaultValue={watch(`adultPassengers.${index}.firstname`)}
-                                    onValueChange={(value) => setValue(`adultPassengers.${index}.firstname`, value)}
-                                    errorMessage={errors?.adultPassengers?.[index]?.firstname?.message}
-                                    isInvalid={!!errors?.adultPassengers?.[index]?.firstname}
-                                    classNames={{
-                                        inputWrapper: "rounded-none",
-                                        mainWrapper: "w-full"
-                                    }}
+                                    value={watch(`adultPassengers.${index}.lastname`)}
+                                    onValueChange={(value) => setValue(`adultPassengers.${index}.lastname`, value)}
+                                    isInvalid={!!errors?.adultPassengers?.[index]?.lastname}
+                                    errorMessage={errors?.adultPassengers?.[index]?.lastname?.message}
+                                    className="rounded-none w-full"
+                                />
+                            </div>
+                            <div className="flex flex-row gap-2 items-center">
+                                <p className="w-[50%]">{t('checkout.date_of_birth')}</p>
+                                <DatePicker
+                                    onChange={(value) => handleDateChange(value, index)}
+                                    placeholder={t('checkout.select_date')}
+                                    isInvalid={!!errors?.adultPassengers?.[index]?.date_of_birth}
+                                    errorMessage={errors?.adultPassengers?.[index]?.date_of_birth?.message}
+                                    defaultValue={parseDateSafely(watch(`adultPassengers.${index}.date_of_birth`))}
                                 />
                             </div>
                         </div>
-                        <div className="flex flex-row gap-2 items-center">
-                            <p className="w-[50%]">{t('checkout.last_name')}</p>
-                            <Input
-                                type="text"
-                                variant="bordered"
-                                defaultValue={watch(`adultPassengers.${index}.lastname`)}
-                                onValueChange={(value) => setValue(`adultPassengers.${index}.lastname`, value)}
-                                errorMessage={errors?.adultPassengers?.[index]?.lastname?.message}
-                                isInvalid={!!errors?.adultPassengers?.[index]?.lastname}
-                                classNames={{
-                                    inputWrapper: "rounded-none",
-                                    mainWrapper: "w-full"
-                                }}
-                            />
-                        </div>
-                        <div className="flex flex-row gap-2 items-center">
-                            <p className="w-[50%]">{t('checkout.date_of_birth')}</p>
-                            <DatePicker
-                                onChange={(value) => setValue(`adultPassengers.${index}.date_of_birth`, moment(value).format('YYYY-MM-DD'))}
-                                variant="underlined"
-                                showMonthAndYearPickers
-                                errorMessage={errors?.adultPassengers?.[index]?.date_of_birth?.message}
-                                isInvalid={!!errors?.adultPassengers?.[index]?.date_of_birth}
-                                defaultValue={watch(`adultPassengers.${index}.date_of_birth`) ? parseDate(watch(`adultPassengers.${index}.date_of_birth`)) : null}
-                            />
-                        </div>
-                    </div>
-                </CardBody>
-            </Card>
-        ))
+                    </CardBody>
+                </Card>
+            ))}
+        </>
     )
 }
 
