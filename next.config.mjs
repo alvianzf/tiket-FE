c4b4628507fe2a86a9f7d98bdd13ed53fbfffead
alvianzf <alvianzf@gmail.com>
2026-03-08 16:35:13 +0700

    perf(fe): add next.config optimizations

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
